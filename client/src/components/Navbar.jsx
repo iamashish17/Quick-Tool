a18604105a25da0a7e4b5d99aa5508bd5fcb973a
iamashish17 <acharya2074@gmail.com>
2025-09-02 14:09:12 +0545

    added sidebar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,13 +11,13 @@ const Navbar = () => {
     const { openSignIn } = useClerk()
 
     return (
-    <div className='fixed z-5 w-full background-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer'>
+    <div className='fixed z-5 w-full backdrop-blur-2xl flex justify-between items-center py-3 px-4 sm:px-20 xl:px-32 cursor-pointer'>
       <img src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer' onClick={()=>
         navigate('/')}/>
 
         {
             user ? <UserButton /> : (
-            <button onClick={openSignIn} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-blue-600 text-white px-10 py-2.5'>
+            <button onClick={openSignIn} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'>
             Get Started <ArrowRight className='w-4 h-4'/>
         </button>
             )
